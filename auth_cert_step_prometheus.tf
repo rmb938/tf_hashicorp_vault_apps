f4c7bd92dddd93dc1addd2ad9e245ea9635d022f
Ryan Belgrave <ryan@belgrave.me>
2025-02-12 17:06:07 -0600

    add prometheus

---
+++ auth_cert_step_prometheus.tf
@@ -0,0 +1,54 @@
+resource "vault_policy" "prometheus" {
+  name = "prometheus"
+
+  policy = <<EOT
+path "${local.consul_mount_path}/creds/prometheus" {
+  capabilities = ["read"]
+}
+EOT
+}
+
+resource "vault_cert_auth_backend_role" "prometheus" {
+  backend = local.step_cert_auth_path
+
+  name         = vault_policy.prometheus.name
+  display_name = vault_policy.prometheus.name
+
+  certificate = file("/usr/local/share/ca-certificates/smallstep-homelab-prod.crt")
+
+  allowed_common_names = [
+    "prometheus.us-homelab1.hl.rmb938.me",
+  ]
+  token_bound_cidrs = [
+    "192.168.23.58/32",
+  ]
+
+  token_policies = [
+    "default",
+    vault_policy.default_apps.name,
+    vault_policy.prometheus.name,
+  ]
+}
+
+resource "consul_acl_policy" "prometheus" {
+  name  = vault_policy.prometheus.name
+  rules = <<-RULE
+    service_prefix "${vault_policy.prometheus.name}" {
+      policy = "write"
+    }
+    RULE
+}
+
+resource "vault_consul_secret_backend_role" "prometheus" {
+  name    = vault_policy.prometheus.name
+  backend = local.consul_mount_path
+
+  consul_policies = [
+    consul_acl_policy.default_apps.name,
+    consul_acl_policy.prometheus.name,
+  ]
+
+  node_identities = [
+    for name in vault_cert_auth_backend_role.prometheus.allowed_common_names : "${split(".", "${name}")[0]}:hl-us-homelab1"
+  ]
+}
