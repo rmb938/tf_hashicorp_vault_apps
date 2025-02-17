a225cb949d3d11f4ba694231c4ce372341c07b67
Ryan Belgrave <ryan@belgrave.me>
2025-02-17 11:31:32 -0600

    t2

--- auth_cert_step_haproxy-t2.tf
+++ auth_cert_step_haproxy-t2.tf
@@ -1,4 +1,4 @@
-module "haproxy-t1" {
+module "haproxy-t2" {
   source = "./modules/step_cert_app"
 
   secret_mount_path   = local.secret_mount_path
@@ -6,14 +6,14 @@ module "haproxy-t1" {
   consul_mount_path   = local.consul_mount_path
   consul_datacenter   = local.consul_datacenter_homelab
 
-  name = "haproxy-t1"
+  name = "haproxy-t2"
   allowed_common_names = [
-    "haproxy-t1-1.us-homelab1.hl.rmb938.me",
-    "haproxy-t1-2.us-homelab1.hl.rmb938.me",
+    "haproxy-t2-1.us-homelab1.hl.rmb938.me",
+    "haproxy-t2-2.us-homelab1.hl.rmb938.me",
   ]
   token_bound_cidrs = [
-    "192.168.23.47/32",
-    "192.168.23.48/32",
+    "192.168.23.49/32",
+    "192.168.23.50/32",
   ]
 
   vault_policy_extra = <<EOT
