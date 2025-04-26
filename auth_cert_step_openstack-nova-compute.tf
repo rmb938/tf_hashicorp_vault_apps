23858a923c8f6936c73a513af877225e9ab417dc
Ryan Belgrave <ryan@belgrave.me>
2025-04-26 11:08:07 -0500

    add nova compute

---
+++ auth_cert_step_openstack-nova-compute.tf
@@ -0,0 +1,16 @@
+module "openstack-nova-compute" {
+  source = "./modules/step_cert_app"
+
+  secret_mount_path   = local.secret_mount_path
+  step_cert_auth_path = local.step_cert_auth_path
+  consul_mount_path   = local.consul_mount_path
+  consul_datacenter   = local.consul_datacenter_homelab
+
+  name = "openstack-nova-compute"
+  allowed_common_names = [
+    "roxas.rmb938.me"
+  ]
+  token_bound_cidrs = [
+    "192.168.23.12/32",
+  ]
+}
