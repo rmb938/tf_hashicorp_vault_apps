49c24859747eb028629b164ace45be1750da0d7a
Ryan Belgrave <ryan@belgrave.me>
2025-05-08 18:25:15 -0500

    add devstack

---
+++ auth_cert_step_devstack.tf
@@ -0,0 +1,16 @@
+module "devstack" {
+  source = "./modules/step_cert_app"
+
+  secret_mount_path   = local.secret_mount_path
+  step_cert_auth_path = local.step_cert_auth_path
+  consul_mount_path   = local.consul_mount_path
+  consul_datacenter   = local.consul_datacenter_homelab
+
+  name = "devstack"
+  allowed_common_names = [
+    "devstack.us-homelab1.hl.rmb938.me",
+  ]
+  token_bound_cidrs = [
+    "192.168.23.101/32",
+  ]
+}
